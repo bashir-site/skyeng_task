ba63917f3a914392697ffafdd10984e5f346c7aa
bashir-site <bashir.gaytukiev@gmail.com>
2024-10-14 02:07:44 +0300

    add docker-compose.yml and Dockerfile

--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*vsly-d5*=$flw&5iiobshv7^-gvw-fxh6_*^5z8im4ai0i(7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
